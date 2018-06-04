670fa788a7988fe38424f0bd67cdfea1cc75b01c
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-04 18:25:48 +0530

    unit test case

--- src/test/java/refactoring/MovieRentalTest.java
+++ src/test/java/refactoring/MovieRentalTest.java
@@ -34,4 +34,34 @@ public class MovieRentalTest {
                 "Amount owed is 4.0\n" +
                 "You earned 2 frequent renter points",customer.statement());
     }
+
+    @Test
+    public void returnFrequency2ForRentingNewReleaseMovieMoreThanOneDay() {
+        Movie deadPool = new Movie("Dead Pool",1);
+        Rental rental = new Rental(deadPool,3);
+        Customer customer = new Customer("Jayanthi");
+        customer.addRental(rental);
+
+        assertEquals("Rental record for Jayanthi\n" +
+                "\tDead Pool\t9.0\n" +
+                "Amount owed is 9.0\n" +
+                "You earned 2 frequent renter points",customer.statement());
+    }
+
+    @Test
+    public void returnAmountForRentingOneChildrenMovieAndOneRegularMovie() {
+        Movie coco = new Movie("COCO",2);
+        Rental cocoRental = new Rental(coco,1);
+        Movie mummy = new Movie("Mummy",0);
+        Rental mummyRental = new Rental(mummy,1);
+        Customer customer = new Customer("Jayanthi");
+        customer.addRental(cocoRental);
+        customer.addRental(mummyRental);
+
+        assertEquals("Rental record for Jayanthi\n" +
+                "\tCOCO\t1.5\n" +
+                "\tMummy\t2.0\n" +
+                "Amount owed is 3.5\n" +
+                "You earned 2 frequent renter points",customer.statement());
+    }
 }
