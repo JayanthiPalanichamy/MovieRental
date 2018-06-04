5c9b97c22605e7f2b9523e832e9b138419982699
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-04 18:15:18 +0530

    Adding unit tests to the project

--- src/test/java/refactoring/MovieRentalTest.java
+++ src/test/java/refactoring/MovieRentalTest.java
@@ -2,7 +2,36 @@ package refactoring;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MovieRentalTest {
     @Test
+    public void returnAmount3ForNewReleasedMovieRentedForOneDay(){
+        Movie deadPool = new Movie("Dead Pool",1);
+        Rental rental = new Rental(deadPool,1);
+        Customer customer = new Customer("Jayanthi");
+        customer.addRental(rental);
 
+        assertEquals("Rental record for Jayanthi\n" +
+                "\tDead Pool\t3.0\n" +
+                "Amount owed is 3.0\n" +
+                "You earned 1 frequent renter points",customer.statement());
+    }
+
+    @Test
+    public void returnAmount3ForRentingTwoRegularMoviesForOneDay() {
+        Movie commando = new Movie("Commando",0);
+        Movie mummy = new Movie("Mummy",0);
+        Rental commandoRental = new Rental(commando,1);
+        Rental mummyRental = new Rental(mummy,1);
+        Customer customer = new Customer("Jayanthi");
+        customer.addRental(commandoRental);
+        customer.addRental(mummyRental);
+
+        assertEquals("Rental record for Jayanthi\n" +
+                "\tCommando\t2.0\n" +
+                "\tMummy\t2.0\n" +
+                "Amount owed is 4.0\n" +
+                "You earned 2 frequent renter points",customer.statement());
+    }
 }
