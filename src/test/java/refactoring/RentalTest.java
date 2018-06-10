aef0e5d46308f0edfd5f2604bf8357728685ee1c
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-10 20:27:24 +0530

    return Rental Amount For Movie Rented

--- src/test/java/refactoring/RentalTest.java
+++ src/test/java/refactoring/RentalTest.java
@@ -6,12 +6,19 @@ import static org.junit.Assert.assertEquals;
 
 public class RentalTest {
     private Movie deadPool;
-
     @Test
     public void returnTitleOfMovieWhichIsRented(){
         deadPool = new NewReleaseMovie("Dead Pool");
         Rental rental = new Rental(deadPool, 1);
 
         assertEquals("Dead Pool",rental.getMovieTitle());
+    }
+
+    @Test
+    public void returnRentalAmountForTheMovieRented() {
+        deadPool = new NewReleaseMovie("Dead Pool");
+        Rental rental = new Rental(deadPool, 1);
+
+        assertEquals(3,rental.getAmount(),0.0001);
     }
 }
